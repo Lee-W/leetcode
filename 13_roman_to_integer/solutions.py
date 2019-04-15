24e92a73c4a5e9757db0df7c95607e5c5a3ec0cf
LeeW <cl87654321@gmail.com>
2019-04-15 09:38:07 +0800

    feat(problem): Solve "13. Roman to Integer"

---
+++ 13_roman_to_integer/solutions.py
@@ -0,0 +1,30 @@
+class Solution:
+    SYMBOL_TO_VALUE = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    def romanToInt(self, s: str) -> int:
+        if len(s) == 1:
+            return self.SYMBOL_TO_VALUE[s]
+
+        digits = [
+            self.SYMBOL_TO_VALUE[c]
+            for c in s
+        ]
+
+        value = 0
+        for d1, d2 in zip(digits[:-1], digits[1:]):
+            if d2 > d1:
+                value -= d1
+            else:
+                value += d1
+
+        if d2:
+            value += d2
+        return value
