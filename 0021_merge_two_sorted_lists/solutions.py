e6c99c430b0a00c4389eefc77c7525a979ecc845
LeeW <cl87654321@gmail.com>
2019-04-18 14:31:05 +0800

    perf(problem): Enhance Problem 0021 speed

--- 0021_merge_two_sorted_lists/solutions.py
+++ 0021_merge_two_sorted_lists/solutions.py
@@ -10,6 +10,37 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+            if l1.val <= l2.val:
+                first_node = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                first_node = ListNode(l2.val)
+                l2 = l2.next
+
+            cur_node = first_node
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    cur_node.next = ListNode(l1.val)
+                    cur_node = cur_node.next
+                    l1 = l1.next
+                else:
+                    cur_node.next = ListNode(l2.val)
+                    cur_node = cur_node.next
+                    l2 = l2.next
+
+            while l1:
+                cur_node.next = ListNode(l1.val)
+                cur_node = cur_node.next
+                l1 = l1.next
+
+            while l2:
+                cur_node.next = ListNode(l2.val)
+                cur_node = cur_node.next
+                l2 = l2.next
+
+            return first_node
+
         if not l1 and not l2:
             return None
 
@@ -18,35 +49,3 @@ class Solution:
 
         if not l2:
             return l1
-
-        if l1.val <= l2.val:
-            first_node = ListNode(l1.val)
-            l1 = l1.next
-        else:
-            first_node = ListNode(l2.val)
-            l2 = l2.next
-
-        cur_node = first_node
-        while l1 and l2:
-            if l1.val <= l2.val:
-                cur_node.next = ListNode(l1.val)
-                cur_node = cur_node.next
-                l1 = l1.next
-            else:
-                cur_node.next = ListNode(l2.val)
-                cur_node = cur_node.next
-                l2 = l2.next
-
-        while l1:
-            print(l1.val)
-            cur_node.next = ListNode(l1.val)
-            cur_node = cur_node.next
-            l1 = l1.next
-
-        while l2:
-            print(l2.val)
-            cur_node.next = ListNode(l2.val)
-            cur_node = cur_node.next
-            l2 = l2.next
-
-        return first_node
