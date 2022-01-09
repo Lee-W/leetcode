fb1ae03e1d0ef8daaa5b6f08b724ba86a682a499
Wei Lee <weilee.rx@gmail.com>
2022-01-09 11:15:33 +0800

    refactor(66): plus one

--- problem/p_0066_plus_one/solutions.py
+++ problem/p_0066_plus_one/solutions.py
@@ -3,16 +3,18 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        current_index = len(digits) - 1
+        """
+        Runtime: 38 ms (26.52 %)
+        Memory Usage: 14.1 MB (76.04 %)
+        """
+        for d_idx in range(len(digits) - 1, -1, -1):
+            if digits[d_idx] != 9:
+                digits[d_idx] += 1
+                break
+            else:
+                digits[d_idx] = 0
 
-        while digits[current_index] == 9 and current_index > 0:
-            digits[current_index] = 0
-            current_index -= 1
-
-        if digits[current_index] == 9 and current_index == 0:
-            digits[current_index] = 0
+        if not digits[0]:
             digits.insert(0, 1)
-        else:
-            digits[current_index] += 1
 
         return digits
