9662e106bf9dd562fc005d9beb670e245d359497
Wei Lee <weilee.rx@gmail.com>
2022-01-18 09:46:09 +0800

    fix(1051): fix test cases

--- problem/p_1051_height_checker/test_solutions.py
+++ problem/p_1051_height_checker/test_solutions.py
@@ -3,7 +3,9 @@ import pytest
 from .solutions import Solution
 
 
-@pytest.mark.parametrize("value, expected",)
+@pytest.mark.parametrize(
+    "value, expected", (([1, 1, 4, 2, 1, 3], 3), ([5, 1, 2, 3, 4], 5))
+)
 def test_solutions(value, expected):
     solution = Solution()
-    assert solution.func(value) == expected
+    assert solution.heightChecker(value) == expected
