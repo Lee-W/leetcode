a17543ec16e19a54e3e0f9f3b321e4162b634c03
Wei Lee <weilee.rx@gmail.com>
2022-02-22 10:44:55 +0800

    problem(102): Binary Tree Level Order Traversal

---
+++ problem/p_0102_binary_tree_level_order_traversal/solutions.py
@@ -0,0 +1,37 @@
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    """
+    Runtime: 36 ms (84.39 %)
+    Memory Usage: 14.2 MB (81.21 %)
+    """
+
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        queue = [root]
+        result = []
+        while queue:
+            tmp_level = [node for node in queue]
+            tmp_leval_values = []
+            queue = []
+            for node in tmp_level:
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+                tmp_leval_values.append(node.val)
+
+            result.append(tmp_leval_values)
+        return result
